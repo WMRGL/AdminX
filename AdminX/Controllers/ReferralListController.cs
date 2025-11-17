8fbde67a63eed2bb2e7f388b3d16d21e15ef21a2
LOXTON, Martin (BIRMINGHAM WOMEN'S AND CHILDREN'S NHS FOUNDATION TRUST) <martin.loxton@nhs.net>
2025-11-17 09:32:41 +0000

    referrals list

---
+++ AdminX/Controllers/ReferralListController.cs
@@ -0,0 +1,28 @@
+﻿using AdminX.ViewModels;
+using ClinicalXPDataConnections.Data;
+using ClinicalXPDataConnections.Meta;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminX.Controllers
+{
+    public class ReferralListController : Controller
+    {
+        private readonly ClinicalContext _context;
+        private readonly ReferralData _referralData;
+        private readonly ReferralListVM _rvm;
+
+        public ReferralListController(ClinicalContext context)
+        {
+            _context = context;
+            _referralData = new ReferralData(_context);
+            _rvm = new ReferralListVM();
+        }
+
+        public IActionResult Index()
+        {            
+            _rvm.referralList = _referralData.GetUnassignedReferrals();
+
+            return View(_rvm);
+        }
+    }
+}
